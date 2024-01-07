4144ee5e4d1b60c44c51fcd940703a50fa52f857
TiavinaMalalaniaina <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-07 06:43:48 +0300

    initproject

---
+++ src/main/java/mg/company/varotravam/VarotravamApplication.java
@@ -0,0 +1,13 @@
+package mg.company.varotravam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarotravamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarotravamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/company/varotravam/VarotravamApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.company.varotravam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarotravamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
