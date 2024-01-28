59b417ec8c434acb15024c20b30d3214c6e48b17
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-28 23:47:02 +0300

    remove mongo for test

--- src/main/java/mg/company/varotravam/models/Conversation.java
+++ src/main/java/mg/company/varotravam/models/Conversation.java
@@ -1,9 +1,9 @@
 package mg.company.varotravam.models;
 
-import com.mongodb.client.*;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Updates;
-import org.bson.Document;
+// import com.mongodb.client.*;
+// import com.mongodb.client.model.Filters;
+// import com.mongodb.client.model.Updates;
+// import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,77 +21,77 @@ public class Conversation {
     private static final String DATABASE_NAME = "varotravam";
     private static final String COLLECTION_NAME = "conversation";
 
-    public static void getCollection() {
-        try (MongoClient mongoClient = MongoClients.create("mongodb://monorail.proxy.rlwy.net:51470")) {
-            // Sélectionner une base de données
-            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+    // public static void getCollection() {
+    //     try (MongoClient mongoClient = MongoClients.create("mongodb://monorail.proxy.rlwy.net:51470")) {
+    //         // Sélectionner une base de données
+    //         MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
 
-            // Sélectionner une collection
-            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+    //         // Sélectionner une collection
+    //         MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
 
-        }
-    }
-
-    public static void create(MongoCollection<Document> collection) {
-        List<Document> messages = new ArrayList<>();
-        messages.add(new Document("acheteur_message", "").append("vendeur_message", ""));
-
-        Document annonceDocument = new Document("annonce_id", 0)
-                .append("acheteur_id", 0)
-                .append("vendeur_id", 0)
-                .append("acheteur_nom", "")
-                .append("vendeur_nom", "")
-                .append("messages", messages);
-
-        collection.insertOne(annonceDocument);
-
-        System.out.println("Annonce créée avec succès.");
-    }
-
-    // public static void read(MongoCollection<Document> collection, int annonceId) {
-    //     System.out.println("Lecture de l'annonce avec critères :");
-    //     collection.find(Filters.and(
-    //             Filters.eq("annonce_id", annonceId)
-    //     )).forEach((Document documentResult) -> System.out.println(documentResult.toJson()));
+    //     }
     // }
 
-    // public static void read(MongoCollection<Document> collection, int annonceId, int acheteurId) {
-    //     System.out.println("Lecture de l'annonce avec critères :");
-    //     collection.find(Filters.and(
-    //             Filters.eq("annonce_id", annonceId),
-    //             Filters.eq("acheteur_id", acheteurId)
-    //     )).forEach((Document documentResult) -> System.out.println(documentResult.toJson()));
+    // public static void create(MongoCollection<Document> collection) {
+    //     List<Document> messages = new ArrayList<>();
+    //     messages.add(new Document("acheteur_message", "").append("vendeur_message", ""));
+
+    //     Document annonceDocument = new Document("annonce_id", 0)
+    //             .append("acheteur_id", 0)
+    //             .append("vendeur_id", 0)
+    //             .append("acheteur_nom", "")
+    //             .append("vendeur_nom", "")
+    //             .append("messages", messages);
+
+    //     collection.insertOne(annonceDocument);
+
+    //     System.out.println("Annonce créée avec succès.");
     // }
 
-    public void addAcheteurMessage(MongoCollection<Document> collection) {
-        System.out.println("Ajout d'un message à l'annonce :");
+    // // public static void read(MongoCollection<Document> collection, int annonceId) {
+    // //     System.out.println("Lecture de l'annonce avec critères :");
+    // //     collection.find(Filters.and(
+    // //             Filters.eq("annonce_id", annonceId)
+    // //     )).forEach((Document documentResult) -> System.out.println(documentResult.toJson()));
+    // // }
 
-        // Ajouter un nouveau message à la liste des messages
-        collection.updateOne(
-            Filters.and(
-                Filters.eq("annonce_id", this.getAnnonceId()),
-                Filters.eq("acheteur_id", this.getAcheteurId())
-            ),
-            Updates.push("messages", new Document("acheteur_message", this.getMessage()).append("vendeur_message", ""))
-        );
+    // // public static void read(MongoCollection<Document> collection, int annonceId, int acheteurId) {
+    // //     System.out.println("Lecture de l'annonce avec critères :");
+    // //     collection.find(Filters.and(
+    // //             Filters.eq("annonce_id", annonceId),
+    // //             Filters.eq("acheteur_id", acheteurId)
+    // //     )).forEach((Document documentResult) -> System.out.println(documentResult.toJson()));
+    // // }
 
-        System.out.println("Message ajouté avec succès.");
-    }
+    // public void addAcheteurMessage(MongoCollection<Document> collection) {
+    //     System.out.println("Ajout d'un message à l'annonce :");
 
-    public void addVendeurMessage(MongoCollection<Document> collection) {
-        System.out.println("Ajout d'un message à l'annonce :");
+    //     // Ajouter un nouveau message à la liste des messages
+    //     collection.updateOne(
+    //         Filters.and(
+    //             Filters.eq("annonce_id", this.getAnnonceId()),
+    //             Filters.eq("acheteur_id", this.getAcheteurId())
+    //         ),
+    //         Updates.push("messages", new Document("acheteur_message", this.getMessage()).append("vendeur_message", ""))
+    //     );
 
-        // Ajouter un nouveau message à la liste des messages
-        collection.updateOne(
-            Filters.and(
-                Filters.eq("annonce_id", this.getAnnonceId()),
-                Filters.eq("acheteur_id", this.getAcheteurId())
-            ),
-            Updates.push("messages", new Document("acheteur_message", "").append("vendeur_message", this.getMessage()))
-        );
+    //     System.out.println("Message ajouté avec succès.");
+    // }
 
-        System.out.println("Message ajouté avec succès.");
-    }
+    // public void addVendeurMessage(MongoCollection<Document> collection) {
+    //     System.out.println("Ajout d'un message à l'annonce :");
+
+    //     // Ajouter un nouveau message à la liste des messages
+    //     collection.updateOne(
+    //         Filters.and(
+    //             Filters.eq("annonce_id", this.getAnnonceId()),
+    //             Filters.eq("acheteur_id", this.getAcheteurId())
+    //         ),
+    //         Updates.push("messages", new Document("acheteur_message", "").append("vendeur_message", this.getMessage()))
+    //     );
+
+    //     System.out.println("Message ajouté avec succès.");
+    // }
 
     public int getAnnonceId() {
         return annonceId;
