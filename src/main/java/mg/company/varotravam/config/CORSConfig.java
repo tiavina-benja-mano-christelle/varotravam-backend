d1dab712cb5ac9a76c608d81b11ce570c08a11fd
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-29 08:58:34 +0300

    Update CORSConfig.java

--- src/main/java/mg/company/varotravam/config/CORSConfig.java
+++ src/main/java/mg/company/varotravam/config/CORSConfig.java
@@ -3,17 +3,15 @@ package mg.company.varotravam.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CORSConfig {
-    @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
-	}
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
