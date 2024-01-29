af4a7a93621bcd9de33312bfe7835cac1ea83b02
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-29 14:38:25 +0300

    add cors to transmission

--- src/main/java/mg/company/varotravam/controllers/TransmissionController.java
+++ src/main/java/mg/company/varotravam/controllers/TransmissionController.java
@@ -12,6 +12,7 @@ import mg.company.varotravam.utils.JWTtokens;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/transmissions")
+@CrossOrigin
 public class TransmissionController extends MonController {
 
     @GetMapping("/pages")
