fdaabcfe7a9b87c4d1bfaa10b3d8ec4414cf59a8
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-29 08:42:23 +0300

    Update CORSConfig.java

--- src/main/java/mg/company/varotravam/config/CORSConfig.java
+++ src/main/java/mg/company/varotravam/config/CORSConfig.java
@@ -12,10 +12,7 @@ public class CORSConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedOriginPatterns("*")
-					.allowedMethods("*")
-					.allowedHeaders("*");
+				registry.addMapping("/**");
 			}
 		};
 	}
