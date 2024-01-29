c3020191153a5c8eb3ceffd1598766f298db9d03
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-29 10:52:29 +0300

    Update CORSConfig.java

--- src/main/java/mg/company/varotravam/config/CORSConfig.java
+++ src/main/java/mg/company/varotravam/config/CORSConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
 
-public class CORSConfig {
-
-    // @Override
-    // public void addCorsMappings(CorsRegistry registry) {
-    //     registry.addMapping("/**")
-    //         .allowedMethods("*")
-    //         .allowedHeaders("*")
-    //         .allowedOriginPatterns("https://varotravam-admin.netlify.app")
-    //         .allowCredentials(true);
-    // }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowedOriginPatterns("https://varotravam-admin.netlify.app")
+            .allowCredentials(true);
+    }
 }
