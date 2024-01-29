75fd753a22edabd6cc13eae678c02c47e030dba8
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-29 08:17:26 +0300

    update

--- src/main/java/mg/company/varotravam/config/CORSConfig.java
+++ src/main/java/mg/company/varotravam/config/CORSConfig.java
@@ -13,15 +13,15 @@ public class CORSConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**")
-					.allowedOrigins("https://varotravam-admin.netlify.app")
+					.allowedOriginPatterns("*")
 					.allowedMethods("GET", "POST", "PUT", "DELETE")
-					.allowedHeaders("Authorization", "Content-Type")
+					.allowedHeaders("*")
 					.allowCredentials(true)
 					.maxAge(3600);
 				registry.addMapping("/authentification/**")
-					.allowedOrigins("https://varotravam-admin.netlify.app")
+					.allowedOriginPatterns("*")
 					.allowedMethods("GET", "POST", "PUT", "DELETE")
-					.allowedHeaders("Authorization", "Content-Type")
+					.allowedHeaders("*")
 					.allowCredentials(true)
 					.maxAge(3600);
 			}
