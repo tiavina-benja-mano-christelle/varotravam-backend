0dc8bc64d69681b36609899769de6bbcd2873331
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-29 09:25:08 +0300

    Update CORSConfig.java

--- src/main/java/mg/company/varotravam/config/CORSConfig.java
+++ src/main/java/mg/company/varotravam/config/CORSConfig.java
@@ -2,20 +2,37 @@ package mg.company.varotravam.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CORSConfig {
-    @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedOrigins("https://varotravam-admin.netlify.app")
-					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-			}
-		};
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.addAllowedOrigin("*");
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
+
+    // @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// 	return new WebMvcConfigurer() {
+	// 		@Override
+	// 		public void addCorsMappings(CorsRegistry registry) {
+	// 			registry.addMapping("/**")
+	// 				.allowedOrigins("https://varotravam-admin.netlify.app")
+	// 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	// 		}
+	// 	};
+	// }
+
 }
