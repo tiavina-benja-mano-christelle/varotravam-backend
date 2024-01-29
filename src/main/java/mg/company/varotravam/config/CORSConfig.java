6654f69e8780178f4e24757a370afe94dac8eba0
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-29 14:24:42 +0300

    finalizing backoffice

--- src/main/java/mg/company/varotravam/config/CORSConfig.java
+++ src/main/java/mg/company/varotravam/config/CORSConfig.java
@@ -4,20 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CORSConfig implements WebMvcConfigurer {
+// @Configuration
+public class CORSConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedMethods("*")
-            .allowedHeaders("*")
-            .allowedOriginPatterns("https://varotravam-admin.netlify.app")
-            .allowCredentials(true);
-        registry.addMapping("/**")
-            .allowedMethods("*")
-            .allowedHeaders("*")
-            .allowedOriginPatterns("http://localhost:3000")
-            .allowCredentials(true);
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //         .allowedMethods("*")
+    //         .allowedHeaders("*")
+    //         .allowedOriginPatterns("https://varotravam-admin.netlify.app")
+    //         .allowCredentials(true);
+    //     registry.addMapping("/**")
+    //         .allowedMethods("*")
+    //         .allowedHeaders("*")
+    //         .allowedOriginPatterns("http://localhost:3000")
+    //         .allowCredentials(true);
+    // }
 }
--- src/main/java/mg/company/varotravam/controllers/MessagesController.java
+++ src/main/java/mg/company/varotravam/controllers/MessagesController.java
@@ -1,16 +1,19 @@
 package mg.company.varotravam.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.bson.Document;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.bson.Document;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// import com.mongodb.client.MongoClient;
-// import com.mongodb.client.MongoClients;
-// import com.mongodb.client.MongoCollection;
-// import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import mg.company.varotravam.models.Conversation;
 
@@ -18,46 +21,46 @@ import mg.company.varotravam.models.Conversation;
 @RequestMapping("/api/v1/messages")
 @CrossOrigin
 public class MessagesController {
-    // private static final String CONNECTION_STRING = "monorail.proxy.rlwy.net:51470";
-    // private static final String DATABASE_NAME = "varotravam";
-    // private static final String COLLECTION_NAME = "conversation";
+    private static final String CONNECTION_STRING = "monorail.proxy.rlwy.net:51470";
+    private static final String DATABASE_NAME = "varotravam";
+    private static final String COLLECTION_NAME = "conversation";
 
-    // // @GetMapping
-    // // public String read(@RequestParam int annonceId) {
-    // //     try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
-    // //         MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-    // //         MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
-    // //         Conversation.read(collection, annonceId, 1);
-    // //     }
-    // //     return null;
-    // // }
+    @GetMapping
+    public String read(@RequestParam int annonceId) {
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+            Conversation.read(collection, annonceId, 1);
+        }
+        return null;
+    }
 
-    // // @GetMapping("/{acheteurId}")
-    // // public String read(@RequestParam int annonceId, @PathVariable int acheteurId) {
-    // //     try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
-    // //         MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-    // //         MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
-    // //         Conversation.read(collection, 1, 1);
-    // //     }
-    // //     return null;
-    // // }
+    @GetMapping("/{acheteurId}")
+    public String read(@RequestParam int annonceId, @PathVariable int acheteurId) {
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+            Conversation.read(collection, 1, 1);
+        }
+        return null;
+    }
 
-    // @PostMapping("/acheteur")
-    // public void addAcheteurMessage(@RequestBody Conversation conversation) {
-    //     try (MongoClient mongoClient = MongoClients.create(CONNECTION_STRING)) {
-    //         MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-    //         MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
-    //         conversation.addAcheteurMessage(collection);
-    //     }
-    // }
+    @PostMapping("/acheteur")
+    public void addAcheteurMessage(@RequestBody Conversation conversation) {
+        try (MongoClient mongoClient = MongoClients.create(CONNECTION_STRING)) {
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+            conversation.addAcheteurMessage(collection);
+        }
+    }
 
-    // @PostMapping("/vendeur")
-    // public void addVendeurMessage(@RequestBody Conversation conversation) {
-    //     try (MongoClient mongoClient = MongoClients.create(CONNECTION_STRING)) {
-    //         MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-    //         MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
-    //         conversation.addVendeurMessage(collection);
-    //     }
-    // }
+    @PostMapping("/vendeur")
+    public void addVendeurMessage(@RequestBody Conversation conversation) {
+        try (MongoClient mongoClient = MongoClients.create(CONNECTION_STRING)) {
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+            conversation.addVendeurMessage(collection);
+        }
+    }
 
 }
